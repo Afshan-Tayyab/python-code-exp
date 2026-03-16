86ac3973029f3c8898a1aa035d4514414718a4bc
Afshan-Tayyab <tayyaba044705@gmail.com>
2026-03-16 07:24:10 +0530

    Updated Gemini API key loading and fixed app

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ from google import genai
 import os
 from dotenv import load_dotenv
 
-# Load environment variables
 load_dotenv()
 
-# Get API key
 api_key = os.getenv("GEMINI_API_KEY")
 
-# Create client
 client = genai.Client(api_key=api_key)
 
 st.title("🐍 Python Code Explainer")
